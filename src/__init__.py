c06a82e70e2d1410e87276e689a66e07df1ccbd6
The Kintsugi C0des <157332486+Saranoah@users.noreply.github.com>
2025-10-05 08:05:31 +0300

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# Make 'src' a package and ensure relative imports work correctly
-import os
-import sys
-
-# Add the parent directory to sys.path so 'src' can be imported anywhere
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
